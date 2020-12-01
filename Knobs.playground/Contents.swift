bd3d8fd98f89b94f3358d65b2ceec4f660cf4b4d
Brad Howes <bradhowes@mac.com>
2020-12-01 17:43:17 +0100

    New playground

---
+++ Knobs.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+import Knob
+
+class MyViewController : UIViewController {
+
+    let knob = Knob()
+    let label = UILabel()
+
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+        view.addSubview(knob)
+        view.addSubview(label)
+
+        knob.trackLineWidth = 10.0
+        knob.progressLineWidth = 6.0
+        knob.indicatorLineWidth = 6.0
+        knob.setValue(0.3)
+        knob.touchSensitivity = 2.0
+        knob.tickCount = 5
+        knob.tickLineColor = .blue
+        knob.tickLineLength = 0.2
+
+        knob.addTarget(self, action: #selector(update), for: .valueChanged)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size: CGFloat = 120.0
+
+        knob.frame = CGRect(x: view.bounds.midX - size / 2, y: view.bounds.midY - size / 2, width: size, height: size)
+        view.addSubview(knob)
+
+        label.frame = CGRect(x: view.bounds.midX - 60, y: view.bounds.midY + size / 2 + 20, width: 120, height: 40)
+        label.text = "\(knob.value)"
+
+    }
+
+    @IBAction func update(_ sender: Any) {
+        label.text = "\(knob.value)"
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
