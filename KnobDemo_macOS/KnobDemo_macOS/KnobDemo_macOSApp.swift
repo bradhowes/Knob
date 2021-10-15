e893bf3b5cbd60a863d1d5426f16767adb4c9f7f
Brad Howes <bradhowes@mac.com>
2021-10-15 16:26:19 +0200

    New macOS demo app

---
+++ KnobDemo_macOS/KnobDemo_macOS/KnobDemo_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnobDemo_macOSApp.swift
+//  KnobDemo_macOS
+//
+//  Created by Brad Howes on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct KnobDemo_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
